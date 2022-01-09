afac412954cf832b16bceaa03abb8b8b8f961518
Auras-Projects <mohammedmahmud2007@gmail.com>
2022-01-08 23:25:48 -0500

    Started mobile integration.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,13 @@ const Home: NextPage = () => {
     <>
     
       <div className='signinbox-middle'>
-      <h1>Sign In</h1>
-      <form style={{padding:"1.5em"}}>
-      <input type="text" placeholder='Email' /> <br />
-      <input type="text" placeholder='Username'/>
-      <input type="password" placeholder='Password' /> <br />
-      <button className='signin-btn'>Log In</button>
-      
-      </form>
+        <h1>Sign In</h1>
+        <form style={{padding:"1.5em"}}>
+          <input type="email" placeholder='Email' /> <br />
+          <input type="text" placeholder='Username'/> <br />
+          <input type="password" placeholder='Password' /> <br />
+          <button className='signin-btn'>Log In</button>
+        </form>
       </div>
     </>
     
