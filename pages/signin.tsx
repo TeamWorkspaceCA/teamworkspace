dcfb4cc49c2978a0f78d211dd840a7f81857e022
Auras-Projects <mohammedmahmud2007@gmail.com>
2022-01-09 23:16:23 -0500

    Finished front end for sign in

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -6,14 +6,21 @@ const signin: NextPage = () => {
     <>
       <div className='signinbox-middle'>
         <h1>Sign In</h1>
-        <form style={{padding:"0 1.5em 1.5em 1.5em"}}>
+        <form style={{padding:"1.5em"}}>
             <input type="text" placeholder='Email or Username' />
             <input type="password" placeholder='Password' /> 
             <div className='btn--link'>Forgot Password?</div>
             <div className='signin-btn'>Sign In</div>
             <div className='btn--link' style={{textAlign: 'center'}}>Don't have an account? Sign up here</div>
         </form>
-        <div>
+      </div>
+    </>
+  )
+}
+
+export default signin
+
+/* <div>
             <p style={{fontSize: '0.7em'}}>Or sign in with...</p>
 
             <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}>
@@ -27,13 +34,4 @@ const signin: NextPage = () => {
                 </div>
                 <div className='btn--icon'>
                     <FaMicrosoft />
-                    <span>Microsoft</span>
-                </div>
-            </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
-export default signin
+                    <span>Microsoft</span> */
