1ae3fb0866d726ab55b5202449f2d17eec406c4f
EndowTheGreat <polymorphic12@protonmail.com>
2022-01-09 21:12:32 -0800

    fixed registration page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,28 +1,26 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
+import Link from "next/link";
+import type { NextPage } from "next";
 
 const signup: NextPage = () => {
   return (
     <>
-      <div className='signinbox-middle'>
+      <div className="signupbox-middle">
         <h1>Sign Up</h1>
-        <form style={{padding:"1.5em"}}>
-            <input type="text" placeholder='First Name' />
-            <input type="text" placeholder='Last Name' />
-            <input type="email" placeholder='Email' />
-            <input type="phone" placeholder='Phone Number' />
-            <input type="text" placeholder='Username' />
-            <input type="password" placeholder='Password' /> 
-            <div className='signin-btn'>Sign Up</div>
-            <Link href={'signin'}>
-                <div className='btn--link' style={{textAlign: 'center'}}>
-                    Already have an account? Sign in here
-                </div>
-            </Link>  
+        <form style={{ padding: "1.5em" }}>
+          <input type="text" placeholder="Full Legal Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <div className="signin-btn">Sign Up</div>
+          <Link href={"signin"}>
+            <div className="btn--link" style={{ textAlign: "center" }}>
+              Already have an account? Sign in here
+            </div>
+          </Link>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default signup
+export default signup;
