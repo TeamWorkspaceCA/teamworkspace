fbda8a6f87e008e5c36a9b54b40a26a67380943a
EndowTheGreat <polymorphic12@protonmail.com>
2022-02-09 17:31:11 -0800

    updated heading styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,94 +1,126 @@
-import React, { FC, useEffect, useState } from 'react'
-import GetDeviceSize, { deviceSize } from '../lib/getDeviceSize';
-import {FaBars} from 'react-icons/fa'
-import Link from 'next/link'
+import GetDeviceSize, { deviceSize } from "../lib/getDeviceSize";
+import React, { FC, useEffect, useState } from "react";
+
+import { FaBars } from "react-icons/fa";
+import Link from "next/link";
 
 interface NavbarLink {
-    text: string;
-    float?: any;
-    href: string;
-    ChangeNavbarMenu?: any;
+  text: string;
+  float?: any;
+  href: string;
+  ChangeNavbarMenu?: any;
 }
 
 const Navbar = () => {
-    //let deviceSize: deviceSize = 'pc';
-    let [deviceSize, setDeviceSize] = useState<deviceSize>('pc');
+  //let deviceSize: deviceSize = 'pc';
+  let [deviceSize, setDeviceSize] = useState<deviceSize>("pc");
 
-    let [links, setLinks] = useState<NavbarLink[]>([
-        {text: 'Sign In', float: 'right', href:"/signin"},
-        {text: 'Sign Up', float: 'right', href:"/signup"},
-        {text: 'Contact Us', float: 'right', href:"/contact"},
-        {text: 'Prices', float: 'left', href:"/pricing"}
-    ])
+  let [links, setLinks] = useState<NavbarLink[]>([
+    { text: "Sign In", float: "right", href: "/signin" },
+    { text: "Sign Up", float: "right", href: "/signup" },
+    { text: "Contact Us", float: "right", href: "/contact" },
+    { text: "Prices", float: "left", href: "/pricing" },
+  ]);
 
-    let [navbarOpenState, setNavbarOpenState] = useState<boolean>(true);
+  let [navbarOpenState, setNavbarOpenState] = useState<boolean>(true);
 
+  useEffect(() => {
+    setInterval(() => {
+      if (typeof window != "undefined") {
+        setDeviceSize(GetDeviceSize(window));
+      }
+    }, 50);
+  }, []);
 
-    useEffect(()=>{
-        setInterval(() => {
-            if(typeof window != 'undefined') {
-                setDeviceSize(GetDeviceSize(window));
-            }
-        }, 50);
-    }, []);
+  let ChangeNavbarMenu = () => {
+    setNavbarOpenState(!navbarOpenState);
 
-    let ChangeNavbarMenu = () => {
-        setNavbarOpenState(!navbarOpenState);
-        
-        const menu: any = document.querySelector('.navbar-menu');
+    const menu: any = document.querySelector(".navbar-menu");
 
-        menu.style.animation = `slide-${navbarOpenState ? 'in' : 'out'} 1s infinite`;
+    menu.style.animation = `slide-${
+      navbarOpenState ? "in" : "out"
+    } 1s infinite`;
 
-        setTimeout(() => {
-            menu.style.animation = '';
-            menu.style.left = `${navbarOpenState ? '0' : '-100%'}`
-        }, 1000)
-    }
+    setTimeout(() => {
+      menu.style.animation = "";
+      menu.style.left = `${navbarOpenState ? "0" : "-100%"}`;
+    }, 1000);
+  };
 
-    return (
-        <>
-            <div className='navbar'>
-                <Link href={'/'}>
-                    <div className='navbar-btn' style={{float: deviceSize == 'pc' ? 'left' : 'right'}}>Home</div>
-                </Link>
+  return (
+    <>
+      <div className="navbar">
+        <Link href={"/"}>
+          <div
+            className="navbar-btn"
+            style={{ float: deviceSize == "pc" ? "left" : "right" }}
+          >
+            Home
+          </div>
+        </Link>
 
-                {deviceSize == 'pc' ? (
-                    <>
-                        {
-                            links.map(x => <NavbarButton ChangeNavbarMenu={()=>{}} text={x.text} float={x.float} href={x.href}/>)
-                        }
-                    </>
-                ) : <>
-                    <div className='navbar-btn' onClick={ChangeNavbarMenu} style={{float: 'left'}}>
-                        <FaBars/>
-                    </div>
-                </>}
+        {deviceSize == "pc" ? (
+          <>
+            {links.map((x) => (
+              <NavbarButton
+                ChangeNavbarMenu={() => {}}
+                text={x.text}
+                float={x.float}
+                href={x.href}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            <div
+              className="navbar-btn"
+              onClick={ChangeNavbarMenu}
+              style={{ float: "left" }}
+            >
+              <FaBars />
             </div>
-            
-            
-            <div className="navbar-menu">
-                <div className="navbar-items">
-                {
-                    links.map(x => {
-                        return <>
-                            <NavbarButton ChangeNavbarMenu={ChangeNavbarMenu} text={x.text} float={''} href={x.href}/>
-                        </>
-                    })
-                }
-                </div>
-            </div>
-            
-        </>
-    )
-}
+          </>
+        )}
+      </div>
 
-export default Navbar
-
-
-const NavbarButton:FC<NavbarLink> = ({text, float, href, ChangeNavbarMenu}) => {
-    return <Link href={href}>
-        <div key={text} onClick={ChangeNavbarMenu}  className='navbar-btn' style={{float: float}}>
-            {text}
+      <div className="navbar-menu">
+        <div className="navbar-items">
+          {links.map((x) => {
+            return (
+              <>
+                <NavbarButton
+                  ChangeNavbarMenu={ChangeNavbarMenu}
+                  text={x.text}
+                  float={""}
+                  href={x.href}
+                />
+              </>
+            );
+          })}
         </div>
-    </Link>;
-}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
+
+const NavbarButton: FC<NavbarLink> = ({
+  text,
+  float,
+  href,
+  ChangeNavbarMenu,
+}) => {
+  return (
+    <Link href={href}>
+      <div
+        key={text}
+        onClick={ChangeNavbarMenu}
+        className="navbar-btn"
+        style={{ float: float }}
+      >
+        {text}
+      </div>
+    </Link>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import type { NextPage } from 'next'
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-   
-      <h1>Welcome to Team Work Space!</h1>
+      <h1 className="heading">Welcome to Team Work Space!</h1>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
